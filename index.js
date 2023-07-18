4a8704efe7d42b84b65e3abf5e3ae49a0ce80c53
Aastha <saxena.aastha91@gmail.com>
2023-07-18 15:01:53 -0400

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    return [...cats,name]
+}
+
+function prependCat(name){
+    return [name,...cats]
+}
+
+function removeLastCat(){
+    let new_array=[...cats]
+    
+    new_array.pop()
+    return new_array
+}
+
+function removeFirstCat(){
+    
+    let new_array=[...cats]
+    
+    new_array.shift()
+    return new_array
+}
